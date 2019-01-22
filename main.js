bdc96f4b90ff48e420274bebe252135eb0ad48b3
oohara-k <cocoa.liker@gmail.com>
2019-01-23 01:22:08 +0900

    slowTest

--- main.js
+++ main.js
@@ -2,30 +2,27 @@ const puppeteer = require('puppeteer');
 const delay = require('delay');
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1200,
-        height:800,
-    })
-    console.log('---------- goto yahoo.co.jp ----------');
-    await page.goto('https://www.yahoo.co.jp/');
-    await delay(1000);
-    await page.screenshot({path: 'yahoo.png'});
-    console.log('---------- wait and click ----------');
-    await Promise.all([
-        page.waitForNavigation({ waitUntil: 'load'}),
-        page.click('#topicsfb > div.topicsindex > ul.emphasis > li:nth-child(1) > a'),
-    ])
+  const browser = await puppeteer.launch({
+    headless:false,
+    // slowMo:50,
+    appMode:true,
+    ignoreHTTPSErrors: true,
+    devtools:true
+  });
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1200,
+    height:800,
+  })
+  await new Promise(resolve => setTimeout(resolve, 1000));
 
-    await page.screenshot({path: 'news.png'});
-
-    console.log('---------- evaluate ----------');
-    const h2Title = await page.evaluate(() =>
-      document.querySelector('h2.newsTitle').textContent
-    );
-    console.log(h2Title);
-    console.log('---------- close ----------');
-
-    await browser.close();
+  const client = await page.target().createCDPSession();
+  await client.send('Network.emulateNetworkConditions',{
+    'offline':false,
+    'downloadThroughput':5 * 1024,
+    'uploadThroughput': 20 * 1024,
+    'latency': 500,
+  })
+  await page.goto('https://tokyo2020shop.jp/user_data/banner/cp/O10571.png',{timeout: 0});
+  await browser.close();
 })();
